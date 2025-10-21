6899bde4327a41fa84e3098fad3b36767d2ffb67
Rodrigo Myotin <rodrigo.myotin@fenoxtec.com.br>
2025-10-21 18:28:27 -0300

    helper

---
+++ Portifolio.Tests/SeedDataLoader.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Portifolio.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portifolio.Tests
+{
+    public static class SeedDataLoader
+    {
+        public static SeedData Load()
+        {
+            var path = Path.Combine(AppContext.BaseDirectory, "SeedData.json");
+            var json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<SeedData>(json)!;
+        }
+    }
+}
